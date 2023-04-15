a9680c9da106faed49256f5779375b4edc32e216
IbrahimMoh <ibrahim9mohamed52@gmail.com>
2023-04-15 19:40:28 +0100

    remove a hardcoded Django secret key

--- cxntext/settings.py
+++ cxntext/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p!d(-p*abd6i#(vp2zcfi42q0atr63f960-am-)3ni)m-ld64o'
+# SECRET_KEY = 'django-insecure-p!d(-p*abd6i#(vp2zcfi42q0atr63f960-am-)3ni)m-ld64o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
