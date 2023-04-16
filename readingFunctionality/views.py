1b815a286eb4212d2933b4a1cd330000618598a8
IbrahimMoh <ibrahim9mohamed52@gmail.com>
2023-04-16 13:42:23 +0100

    add new buildpack

--- readingFunctionality/views.py
+++ readingFunctionality/views.py
@@ -17,7 +17,7 @@ import openai
 # Create your views here.
 
 poppler_path = r"C:\Users\User\Downloads\Release-23.01.0-0\poppler-23.01.0\Library\bin"
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def home(request):
     return render(request,'readingFunctionality/home.html')
