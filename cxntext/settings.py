f4eb1e069626318a86b089c52953f70a61c7b467
IbrahimMoh <ibrahim9mohamed52@gmail.com>
2023-04-16 12:20:24 +0100

    Automatic configuration with django-on-heroku

--- cxntext/settings.py
+++ cxntext/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-p!d(-p*abd6i#(vp2zcfi42q0atr63f960-am-)3ni)m-ld64o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -139,5 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import django_heroku
+import django-on-heroku
 django_heroku.settings(locals())
