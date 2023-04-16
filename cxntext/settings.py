cfc2cedf5272c95975ac7e96c4bf9a3e652776a9
IbrahimMoh <ibrahim9mohamed52@gmail.com>
2023-04-16 12:26:15 +0100

    Automatic configuration with django_on_heroku

--- cxntext/settings.py
+++ cxntext/settings.py
@@ -139,5 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import django-on-heroku
+import django_on_heroku
 django_heroku.settings(locals())
