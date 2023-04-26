504cedad9852129a8826a4e4315686078be13c55
IbrahimMoh <ibrahim9mohamed52@gmail.com>
2023-04-26 19:55:02 +0100

    change directories for poppler and pytesseract

--- readingFunctionality/views.py
+++ readingFunctionality/views.py
@@ -17,10 +17,10 @@ import openai
 
 
 # Create your views here.
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-poppler_path = r"C:\Users\User\Downloads\Release-23.01.0-0\poppler-23.01.0\Library\bin"
-# pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
-# poppler_path = os.getenv("POPPLER_PATH")
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# poppler_path = r"C:\Users\User\Downloads\Release-23.01.0-0\poppler-23.01.0\Library\bin"
+pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
+poppler_path = os.getenv("POPPLER_PATH")
 
 
 def home(request):
